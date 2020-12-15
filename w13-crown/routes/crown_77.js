3cff48764f3cd8ce52a83212f35a7c9344783a43
jacky2522561 <jacky2522561@gmail.com>
2020-12-15 15:03:32 +0800

    1215

--- w13-crown/routes/crown_77.js
+++ w13-crown/routes/crown_77.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('crown_77', { title: 'Hsingtai Chung, 123456789' });
+  res.render('crown_77', { title: 'jacky, 408411477' });
 });
 
 router.get('/shop_77/:product', function (req, res, next) {
   res.render('product_77', {
-    title: 'Hsingtai Chung, 123456789',
+    title: 'jacky, 408411477',
     product: req.params.product,
   });
 });
---
+++ w14-crown_77/routes/crown_77.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.render('crown_77', { title: 'jacky, 408411477' });
+});
+
+router.get('/shop_77/:product', function (req, res, next) {
+  res.render('product_77', {
+    title: 'Hsingtai Chung, 123456789',
+    product: req.params.product,
+  });
+});
+
+module.exports = router;
